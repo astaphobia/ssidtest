eaf3a2c139a8cd9a12e34be47b0f15345629f5a9
astaphobia <ariasta.ketut@icloud.com>
2018-08-12 09:22:15 +0800

    setup pages with redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './styles/App.css';
+import React, {Component} from "react"
+import {connect} from "react-redux"
+import {BrowserRouter, Route} from "react-router-dom"
+
+import "./styles/App.css"
+import SignIn from "./pages/SignIn"
+import Catalogues from "./pages/Catalogues"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <BrowserRouter>
+        <div className="container">
+          <Route path="/" component={Catalogues} />
+          <Route exact path="/signin" component={SignIn} />
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default connect(null, {})(App)
---
+++ src/pages/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class SignIn extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        Sign In
+      </div>
+    )
+  }
+}
+export default SignIn
